00a6f73aa56ee1232836e83999f74fe1a5949292
Kaique <kaiquejuvencio@hotmail.com>
2020-06-03 13:37:48 -0300

    Arrumando bug que o cursor sai do form Donator

--- frontend/src/pages/RegisterDonator/index.js
+++ frontend/src/pages/RegisterDonator/index.js
@@ -9,36 +9,42 @@ import trackDonationApi from '../../services/trackDonationApi';
 export default function RegisterDonator() {
     const [id, setId] = useState('');
     const [donation, setDonation] = useState('');
+    const [donationTest, setDonationTest] = useState('');
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
 
+    const [openRegister, setOpenRegister] = useState('');
+    const [openUpdate, setOpenUpdate] = useState('');
+    const [openDelete, setOpenDelete] = useState('');
+
     const [receivers, setReceiver] = useState([]);
 
     const ReceiversComponent = (note) => {
-        return (
-            <table>
-                <tr>
-                    <th>Id</th>
-                    <th>Doação</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Telefone</th>
-                    <th>Data Cadastro</th>
-                </tr>
-                {receivers.map(receiver => (
+        if (receivers.length != 0) {
+            return (
+                <table>
                     <tr>
-                        <td>{receiver[0]}</td>
-                        <td>{receiver[1]}</td>
-                        <td>{receiver[2]}</td>
-                        <td>{receiver[3]}</td>
-                        <td>{receiver[4]}</td>
-                        <td>{receiver[5]}</td>
+                        <th>Id</th>
+                        <th>Doação</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Telefone</th>
+                        <th>Data Cadastro</th>
                     </tr>
-                ))}
-            </table>
-
-        )
+                    {receivers.map(receiver => (
+                        <tr>
+                            <td>{receiver[0]}</td>
+                            <td>{receiver[1]}</td>
+                            <td>{receiver[2]}</td>
+                            <td>{receiver[3]}</td>
+                            <td>{receiver[4]}</td>
+                            <td>{receiver[5]}</td>
+                        </tr>
+                    ))}
+                </table>
+            )
+        } return (<button className="button" onClick={handleReceiver} type="button">Listar Doadores</button>)
     }
 
     async function handleReceiver() {
@@ -50,8 +56,7 @@ export default function RegisterDonator() {
             alert(err);
         }
     }
-
-
+    
     async function registerDonator() {
         if (donation === '' || email === '' || name === '') {
             alert("Insira as informações necessárias para cadastro!");
@@ -76,6 +81,7 @@ export default function RegisterDonator() {
         }
     }
 
+
     async function deleteDonator() {
         if (id === '') {
             alert("Insira as informações necessárias para cadastro!");
@@ -96,83 +102,111 @@ export default function RegisterDonator() {
                         <img src={compass} className="App-logo" alt="compass" />
                         <h2 className="compass-text">Track Donation IRES</h2>
                     </div>
-
-                    <h1>Cadastrar Doador</h1>
-                    <p>Preencha os campos para cadastrar um Doador.</p>
-
-                    <form onSubmit={registerDonator}>
-                        <input
-                            placeholder="Doação"
-                            value={donation}
-                            onChange={e => setDonation(e.target.value)}
-                        />
-                        <input
-                            placeholder="E-mail"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <input
-                            placeholder="Nome"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                        />
-                        <input
-                            placeholder="Telefone"
-                            value={phone}
-                            onChange={e => setPhone(e.target.value)}
-                        />
-                        <button className="button" type="submit">Cadastrar</button>
-                    </form>
-
-                    <h1>Alterar Doador</h1>
-                    <p>Preencha os campos para fazer a atualização de um Doador.</p>
-                    <form onSubmit={updateDonator}>
-                        <input
-                            placeholder="Id"
-                            value={id}
-                            onChange={e => setId(e.target.value)}
-                        />
-                        <input
-                            placeholder="Doação"
-                            value={donation}
-                            onChange={e => setDonation(e.target.value)}
-                        />
-                        <input
-                            placeholder="E-mail"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <input
-                            placeholder="Nome"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                        />
-                        <input
-                            placeholder="Telefone"
-                            value={phone}
-                            onChange={e => setPhone(e.target.value)}
-                        />
-                        <button className="button" type="submit">Atualizar</button>
-                    </form>
-
-                    <h1>Excluir Doador</h1>
-                    <p>Informe o Id do Doador que deseja excluir.</p>
-                    <form onSubmit={deleteDonator}>
-                        <input
-                            placeholder="Id"
-                            value={id}
-                            onChange={e => setId(e.target.value)}
-                        />
-                        <button className="button" type="submit">Excluir</button>
-                    </form>
-
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
                     Voltar
-                </Link>
+                    </Link>
 
-                    <button className="button" onClick={handleReceiver} type="button">Listar Receptores</button>
-                    <ReceiversComponent />
+                    {(function(){
+                        if (openRegister == true) {
+                            return (
+                                <div>
+                                    <h1>Cadastrar Doador</h1>
+                                    <p>Preencha os campos para cadastrar um Doador.</p>
+                
+                                    <form onSubmit={registerDonator}>
+                                        <input
+                                            placeholder="Doação"
+                                            value={donationTest}
+                                            onChange={e => {setDonationTest(e.target.value)}}
+                                        />
+                                        <input
+                                            placeholder="E-mail"
+                                            value={email}
+                                            onChange={e => setEmail(e.target.value)}
+                                        />
+                                        <input
+                                            placeholder="Nome"
+                                            value={name}
+                                            onChange={e => setName(e.target.value)}
+                                        />
+                                        <input
+                                            placeholder="Telefone"
+                                            value={phone}
+                                            onChange={e => setPhone(e.target.value)}
+                                        />
+                                        <button className="button-intern" type="submit">Cadastrar</button>
+                                        <button className="button-intern" onClick={e => setOpenRegister(false)} type="button">Fechar</button>
+                                    </form>
+                                </div>
+                            )
+                        }
+                        else {
+                            return (<button className="button" onClick={e => setOpenRegister(true)} type="button">Cadastrar Doador</button>)
+                        }
+                    })()}
+
+                    {(function(){
+                        if (openUpdate == true) {
+                            return (
+                                <div>
+                                    <h1>Alterar Doador</h1>
+                                    <p>Preencha os campos para fazer a atualização de um Doador.</p>
+                                    <form onSubmit={updateDonator}>
+                                        <input
+                                            placeholder="Id"
+                                            value={id}
+                                            onChange={e => setId(e.target.value)}
+                                        />
+                                        <input
+                                            placeholder="Doação"
+                                            value={donation}
+                                            onChange={e => setDonation(e.target.value)}
+                                        />
+                                        <input
+                                            placeholder="E-mail"
+                                            value={email}
+                                            onChange={e => setEmail(e.target.value)}
+                                        />
+                                        <input
+                                            placeholder="Nome"
+                                            value={name}
+                                            onChange={e => setName(e.target.value)}
+                                        />
+                                        <input
+                                            placeholder="Telefone"
+                                            value={phone}
+                                            onChange={e => setPhone(e.target.value)}
+                                        />
+                                        <button className="button-intern" type="submit">Enviar</button>
+                                        <button className="button-intern" onClick={e => setOpenUpdate(false)} type="button">Fechar</button>
+                                    </form>
+                                </div>
+                            )
+                        } else { return (<button className="button" onClick={e => setOpenUpdate(true)} type="button">Alterar Doador</button>) }
+                    })()}
+
+                    {(function(){
+                         if(openDelete == true){
+                            return (
+                                <div>
+                                    <h1>Excluir Doador</h1>
+                                    <p>Informe o Id do Doador que deseja excluir.</p>
+                                    <form onSubmit={deleteDonator}>
+                                        <input
+                                            placeholder="Id"
+                                            value={id}
+                                            onChange={e => setId(e.target.value)}
+                                        />
+                                        <button className="button-intern" type="submit">Enviar</button>
+                                        <button className="button-intern" onClick={e => setOpenDelete(false)} type="button">Fechar</button>
+                                    </form>
+                                </div>
+                            )
+                            }else{return(<button className="button" onClick={e => setOpenDelete(true)} type="button">Excluir Doador</button>)}
+                    })()}
+
+                    <ReceiversComponent/>
                 </section>
             </div>
         </div>
