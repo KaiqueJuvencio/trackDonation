e544d150fd54ad1826f869dcc453db229af09bac
Kaique <kaiquejuvencio@hotmail.com>
2020-12-24 00:02:10 -0300

    Integrando ResponseError com BadRequestException

--- backend/src/main/java/com/br/trackDonation/exception/BadRequestException.java
+++ backend/src/main/java/com/br/trackDonation/exception/BadRequestException.java
@@ -1,11 +1,19 @@
 package com.br.trackDonation.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class BadRequestException extends RuntimeException{
 	
 	private static final long serialVersionUID = 1L;
-
+	
+	private ResponseErrorMessage responseErrorMessage;
 	
 	public BadRequestException(String message) {
 		super(message);
+		this.responseErrorMessage = new ResponseErrorMessage(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, message);
+	}
+
+	public ResponseErrorMessage getErrorMessage() {
+		return responseErrorMessage;
 	}
 }
