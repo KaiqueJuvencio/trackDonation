5aab64d3354fbec3a3345ea89bc6af263bbec3bf
Kaique <kaiquejuvencio@hotmail.com>
2021-01-09 21:38:37 -0300

    :dart: Adicionando receiver component

--- frontEndAgunlar/src/app/app.module.ts
+++ frontEndAgunlar/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HeaderComponent } from './header/header.component';
 import { DonatorComponent } from './donator/donator.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
+import { ReceiverComponent } from './receiver/receiver.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DonatorComponent
+    DonatorComponent,
+    ReceiverComponent
   ],
   imports: [
     BrowserModule,
--- frontEndAgunlar/src/app/app.routes.ts
+++ frontEndAgunlar/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router'
 import { DonatorComponent } from './donator/donator.component'
+import { ReceiverComponent } from './receiver/receiver.component'
 
 export const ROUTES: Routes = [
-   {path: 'donator', component: DonatorComponent}
+   {path: 'donator', component: DonatorComponent},
+   {path: 'receiver', component: ReceiverComponent}
 ]
